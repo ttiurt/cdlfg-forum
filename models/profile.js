053d7b3e2e2f4c5ee70d58fd9082670b98798e00
ttiurt <truittzach33@gmail.com>
2023-04-24 15:19:27 -0400

    change show

--- models/profile.js
+++ models/profile.js
@@ -5,11 +5,11 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   username: { type: String, default: "New User" },
   bio: String,
-  actid: { type: String, required: true},
-  battlenet: String,
-  steam: String,
-  xbox: String,
-  psn: String,
+  actid: { type: String, default: 'N/A'},
+  battlenet: { type: String, default: 'N/A' },
+  steam: { type: String, default: 'N/A' },
+  xbox: { type: String, default: 'N/A' },
+  psn: { type: String, default: 'N/A' },
   name: String,
   avatar: String,
 }, {
