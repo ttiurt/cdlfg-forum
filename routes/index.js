1e8599b0a557c2bfa246244efb8bd3cdd470e11d
ttiurt <truittzach33@gmail.com>
2023-04-27 12:10:02 -0400

    profile and home style update

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/home', function (req, res) {
   res.render('index', { title: 'Home Page' })
 })
 
+router.get('/', function(req, res){
+  res.redirect('/home')
+})
+
 export {
   router
 }
